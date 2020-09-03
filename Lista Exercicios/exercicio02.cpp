b8280bccfde081c57de805dfaf916104de337e33
Marcelo Santiago <ms8264712@gmail.com>
2020-09-03 20:46:43 -0300

    Lista de exercícios em c++

---
+++ Lista Exercicios/exercicio02.cpp
@@ -0,0 +1,40 @@
+<<<<<<< HEAD
+#include <iostream>
+using namespace std;
+int main(){
+	cout <<"Exercicio 02"<<endl;
+	int idade;
+	cout <<"Digite a idade do nadador: ";
+	cin >>idade;
+	if(idade>=5 && idade<=7){
+		cout <<"Infantil A"<<endl;
+	}else if(idade>=8 && idade<=10){
+		cout <<"Infantil B"<<endl;
+	}else if(idade>=11 && idade<=13){
+		cout <<"Juvenil A"<<endl;
+	}else if(idade >= 14 && idade <=17){
+		cout <<"Juvenil B"<<endl;
+	}else{
+		cout <<"Senior"<<endl;
+	}	
+=======
+#include <iostream>
+using namespace std;
+int main(){
+	cout <<"Exercicio 02"<<endl;
+	int idade;
+	cout <<"Digite a idade do nadador: ";
+	cin >>idade;
+	if(idade>=5 && idade<=7){
+		cout <<"Infantil A"<<endl;
+	}else if(idade>=8 && idade<=10){
+		cout <<"Infantil B"<<endl;
+	}else if(idade>=11 && idade<=13){
+		cout <<"Juvenil A"<<endl;
+	}else if(idade >= 14 && idade <=17){
+		cout <<"Juvenil B"<<endl;
+	}else{
+		cout <<"Senior"<<endl;
+	}	
+>>>>>>> 28220a29c523e80f9a481f8ae7b09b5fd0b45a7b
+}
